a8067019f87533d9878d1abed684212a537c6341
Emilia Simmons <emilia.simmons@gmail.com>
2020-12-26 13:45:26 -0500

    (fish) Add fzf history search on /

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings
     bind -M insert jk "commandline -f cancel backward-char force-repaint; set fish_bind_mode default"
+    bind -M default / "fzf_history; and set fish_bind_mode insert"
 end
