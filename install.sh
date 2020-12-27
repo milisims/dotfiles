1e86e1f5cf64b979efce05816586ba2689e5c91c
Emilia Simmons <emilia.simmons@gmail.com>
2020-12-26 23:35:15 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit -o pipefail -o noclobber -o nounset
+
+git clone --bare git@github.com:milisims/dotfiles.git ~/.cfg
+function config {
+  git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+echo '/*
+!LICENSE.txt
+!README.md
+!install.sh
+!windows' > ~/.cfg/info/sparse-checkout
+
+config config core.sparseCheckout true
+config config status.showUntrackedFiles no
+
+set +o errexit
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config."
+else
+  echo "Backing up pre-existing dot files."
+  for f in $(config checkout 2>&1 | egrep "\s+\." | awk {'print $1'}); do
+    mkdir -p .config-backup/$(dirname $f)
+    mv $f .config-backup/$f
+  done
+  config checkout
+  echo "Checked out config."
+fi
+
